3f91484262c5ead91414f0c90dd8b302921ba070
candelanoy <44499552+candelanoy@users.noreply.github.com>
2018-11-08 06:22:40 -0300

    uploaded merge function

--- assignment.py
+++ assignment.py
@@ -20,4 +20,23 @@ def bubble(lst):
                 
 #%%
                 
-                
+def merge(left, right):
+    result = []
+
+    while len(left) != 0 and len(right) != 0:
+        if left[0] < right[0]:
+            result.append(left[0])
+            left.pop(0)
+        else:
+            result.append(right[0])
+            right.pop(0)
+            
+    if len(left) == 0:
+        result += right
+    else:
+        result += left
+            
+    return result  
+
+#%%
+    
